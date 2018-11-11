eb8839a4495501a9fa1bc6114d005c9125f4c7ba
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-11-11 21:45:18 +0000

    private messages try

--- app/forms.py
+++ app/forms.py
@@ -55,3 +55,8 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Request Password Reset')
+
+class PrivateMessages(FlaskForm):
+    message = TextAreaField(_l('Message'), validators=[
+        DataRequired(), Length(min=0, max=140)])
+    submit = SubmitField(_l('Submit'))
