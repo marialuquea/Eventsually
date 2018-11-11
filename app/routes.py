f96c1ca82c454a7014e72579f259f96aae0dde8b
Maria Luque <mluqueanguita@gmail.com>
2018-11-11 22:11:16 +0000

    PRIVATE MESSAGES WORK

--- app/routes.py
+++ app/routes.py
@@ -176,9 +176,9 @@ def send_message(recipient):
                       body=form.message.data)
         db.session.add(msg)
         db.session.commit()
-        flash(_('Your message has been sent.'))
+        flash('Your message has been sent.')
         return redirect(url_for('user', username=recipient))
-    return render_template('send_message.html', title=_('Send Message'),
+    return render_template('send_message.html', title='Send Message',
                            form=form, recipient=recipient)
 
 @app.route('/messages')
@@ -189,7 +189,7 @@ def messages():
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
         Message.timestamp.desc()).paginate(
-            page, current_app.config['POSTS_PER_PAGE'], False)
+            page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('messages', page=messages.next_num) \
         if messages.has_next else None
     prev_url = url_for('messages', page=messages.prev_num) \
