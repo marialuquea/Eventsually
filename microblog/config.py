49979a536f38fba0172322c4d6e7ecaf37fd5585
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-11-11 19:03:08 +0000

    email does not work hehe ugh

--- microblog/config.py
+++ microblog/config.py
@@ -7,9 +7,11 @@ class Config(object):
         	'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	POSTS_PER_PAGE = 10
-	ADMINS = ['mluqueanguita@gmail.com']
-	MAIL_SERVER = 'smtp.googlemail.com'
-    	MAIL_PORT = 587
-    	MAIL_USE_TLS = 1
-    	MAIL_USERNAME = 'mluqueanguita@gmail.com'
-    	MAIL_PASSWORD = 'uhramP58'
+    ADMINS = ['mluqueanguita@gmail.com']
+
+	MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+	MAIL_USE_SSL = False
+    MAIL_USERNAME = 'mluqueanguita@gmail.com'
+    MAIL_PASSWORD = 'uhramP58'
