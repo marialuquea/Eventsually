cee6e757b5b2802986a6e71dc6f47e92bd2f80fb
Maria Luque Anguita <40280156@live.napier.ac.uk>
2018-11-12 14:19:00 +0000

    send messages fixed

--- main.py
+++ main.py
@@ -173,8 +173,7 @@ def send_message(recipient):
     user = User.query.filter_by(username=recipient).first_or_404()
     form = PrivateMessages()
     if form.validate_on_submit():
-        msg = Message(author=current_user, recipient=user,
-                      body=form.message.data)
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
         db.session.add(msg)
 	user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
@@ -190,9 +189,7 @@ def messages():
     current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
-    messages = current_user.messages_received.order_by(
-        Message.timestamp.desc()).paginate(
-            page, app.config['POSTS_PER_PAGE'], False)
+    messages = current_user.messages_received.order_by( Message.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('messages', page=messages.next_num) \
         if messages.has_next else None
     prev_url = url_for('messages', page=messages.prev_num) \
