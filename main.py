a419841eb0b563b2fb9f4445315ff02aacff8d4a
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-11-21 14:05:29 +0000

    changed main for index.html and hopefully fixed post event css

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def index():
         if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
+    return render_template('index.html', title='Post Event', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -206,7 +206,7 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template("index.html", title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
+    return render_template("index.html", title='Discover new events!', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
