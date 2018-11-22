286370e3a807cd17f1a54f5056d23fc2bde8155b
Maria Luque Anguita <40280156@live.napier.ac.uk>
2018-11-22 17:36:56 +0000

    jhsadnsadljkahsd

--- app/models.py
+++ app/models.py
@@ -6,6 +6,14 @@ from hashlib import md5
 from time import time
 import jwt
 import json
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+engine = create_engine('sqlite://', echo=True)
+Base = declarative_base(bind=engine)
+Session = sessionmaker(bind=engine)
 
 followers = db.Table(
     'followers',
@@ -128,3 +136,5 @@ class Notification(db.Model):
 
     def get_data(self):
         return json.loads(str(self.payload_json))
+
+Base.metadata.create_all()
