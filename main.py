771a316f3608279a8bcc09f7692c3eaeae49a980
Maria Luque Anguita <40280156@live.napier.ac.uk>
2018-11-22 22:48:40 +0000

    delete interest

--- main.py
+++ main.py
@@ -13,15 +13,19 @@ from PIL import Image
 @app.route('/interested/<user_id>/<post_id>')
 @login_required
 def interested(post_id, user_id):
-    post = Post.query.get(post_id)
-    item = UserList(
-    post_id=post_id,
-    user_id=user_id,
-    event_interested=post)
-    db.session.add(item)
-    db.session.commit()
-    flash('You are interested in this event')
-    return redirect(url_for('explore'))
+    if UserList.query.filter_by(post_id=post_id).filter_by(user_id=user_id)[0] is None:
+        post = Post.query.get(post_id)
+        item = UserList(
+        post_id=post_id,
+        user_id=user_id,
+        event_interested=post)
+        db.session.add(item)
+        db.session.commit()
+        flash('You are interested in this event')
+        return redirect(url_for('explore'))
+    else:
+        flash('You are already interested in this event')
+        return redirect(url_for('explore'))
 
 @app.route('/notinterested/<user_id>/<post_id>')
 @login_required
